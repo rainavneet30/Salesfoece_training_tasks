2e9b62f8a19e45342530b52d5b32bb3c5db5cd88
Navneet Rai <95979040+rainavneet30@users.noreply.github.com>
2023-12-08 19:19:57 +0530

    Update index.js

--- src/assingment/index.js
+++ src/assingment/index.js
@@ -11,3 +11,12 @@ for(let i=0;i<companies.length;i++)
   console.log(companies[i].name);
 for(let i=0;i<companies.length;i++)
   console.log(companies[i].hq);
+ //task 2
+function displayInfo() {
+const container = document.body;
+let listItem = '';
+for let i = 0; i < companies.length; i++){
+const company
+companies[i];
+listItem += <li>${company.name} - ${company. hq} - ${company.indiaLocations.join(', ')}</li>;
+container.innerHTML +='<ul>' + listItem + '</ul>';
